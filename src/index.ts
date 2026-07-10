0811f1b07d6d3959a8a34fd3908516e73cad5929
CozyCrypto <fassdavid722@gmail.com>
2026-07-10 23:17:58 +0100

    feat: scaffold security module src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './types';
+export { PolicyEngine } from './Policy/policy';
+export { AuditLogger } from './Audit/audit';
+export { EncryptionEngine } from './Encryption/encryption';
+export { SecretsScanner } from './SecretsScanner/scanner';
+export { SecurityMonitor } from './Monitoring/secmonitor';
+export { IncidentResponse } from './IncidentResponse/incident';
